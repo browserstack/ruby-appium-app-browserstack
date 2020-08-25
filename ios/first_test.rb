d067e595228bcc68b9845a8ebb0f59b472391e12
Neeraj Kumar <kn.neeraj.89@gmail.com>
2020-08-26 03:43:35 +0530

    Updating First test for Android and iOS

--- ios/first_test.rb
+++ ios/first_test.rb
@@ -13,13 +13,34 @@ caps['platformName'] = 'iOS'
 caps['browserstack.debug'] = true
 caps['app'] = 'bs://<hashed app-id>'
 
+caps = {}
+# Set your access credentials
+caps['browserstack.user'] = 'YOUR_USERNAME'
+caps['browserstack.key'] = 'YOUR_ACCESS_KEY'
+
+# Set URL of the application under test
+caps['app'] = 'bs://<app-id>'
+
+# Specify device and os_version for testing
+caps['device'] = "iPhone 11 Pro"
+caps['os_version'] = "13"
+
+# Set other BrowserStack capabilities
+caps['project'] = 'First Ruby project'
+caps['build'] = 'Ruby iOS'
+caps['name'] = 'single_test'
+
+# Initialize the remote Webdriver using BrowserStack remote URL
+# and desired capabilities defined above
 appium_driver = Appium::Driver.new({
 	'caps' => caps,
 	'appium_lib' => {
-		:server_url => "http://#{username}:#{access_key}@hub-cloud.browserstack.com/wd/hub"
+		:server_url => "http://hub-cloud.browserstack.com/wd/hub"
 	}}, true)
 driver = appium_driver.start_driver
 
+# Test case for the BrowserStack sample Android app. 
+# If you have uploaded your app, update the test case here. 
 wait = Selenium::WebDriver::Wait.new(:timeout => 30)
 wait.until { driver.find_element(:accessibility_id, "Text Button").displayed? }
 textButton = driver.find_element(:accessibility_id, "Text Button")
@@ -40,4 +61,5 @@ else
   puts "Test Failed"
 end
 
+# Invoke driver.quit() after the test is done to indicate that the test is completed.
 driver.quit
